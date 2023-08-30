f602e88582feb165aa6eef3d4b12705505217383
mohamed-jebali <mohamedjebali992010@gmail.com>
2023-08-30 15:14:16 +0200

    added api Controller and route api

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Project;
+
+class ProjectController extends Controller
+{
+    public function index (){
+
+
+        $projects = Project::with('technologies','types')->paginate(5);
+
+        return response()->json(
+            [
+                'success'=> true,
+                'results' => $projects,
+            ]
+        );
+    }
+}
