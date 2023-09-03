99b0969daec93c5e60d65b985e849fb2bc7e19f0
mohamed-jebali <mohamedjebali992010@gmail.com>
2023-09-03 12:53:33 +0200

    added search query

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -5,13 +5,21 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Project;
+use App\Models\Type;
+
 
 class ProjectController extends Controller
 {
-    public function index (){
+    public function index (Request  $request){
 
+    
+            if ($request->has('search')){
+                $projects = Project::with('technologies', 'type')->where('title', 'LIKE', '%' . $request->search . '%')->paginate(20);
 
-        $projects = Project::with('technologies','type')->paginate(20);
+            }
+             else {
+                $projects = Project::with('technologies','type')->paginate(20);
+            }
 
         return response()->json(
             [
